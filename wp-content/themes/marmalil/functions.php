beecd90d9dd9e43aba8d5e855bf3c381229f16f3
May Hitchings <mayhitchings@gmail.com>
2023-05-08 07:51:14 -0400

    register and enqueue style

--- wp-content/themes/marmalil/functions.php
+++ wp-content/themes/marmalil/functions.php
@@ -14,7 +14,11 @@ wp_die();
     wp_enqueue_style( 'main-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
     wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime( get_template_directory() . '/assets/js/main.js'), true )
     // wp_enqueue_style( 'blog-css', get_template_directory_uri() . 'blog.css', ['main-css']);
- }
+ 
+    //register style first, then enqueue conditionally, like on a certain page
+    //wp_register_style( 'main-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+   // if (is_archive() ) {wp_enqueue_style('main-css');}
+}
 
  add_action( 'wp_enqueue_scripts', 'marmalil_enqueue_scripts')
  ?>
