187db83297fcb95631b637c61f8e2e37b41f4ad5
May Hitchings <mayhitchings@gmail.com>
2023-05-22 07:56:39 -0400

    add title tag dynamicallly, setting up theme in class marmalil theme

--- wp-content/themes/marmalil/inc/classes/class-marmalil-theme.php
+++ wp-content/themes/marmalil/inc/classes/class-marmalil-theme.php
@@ -14,5 +14,27 @@
  class MARMALIL_THEME {
     use Singleton;
 
+    protected function __construct() {
+
+      //load class
+
+      Assets::get_instance();
+
+      $this->setup_hooks();
+
+    }
+
+    protected function setup_hooks() {
+
+      /* Actions
+      */
+      add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
+
+      public function setup_theme() {
+         add_theme_support( 'title-tag' );
+         
+      }
+
+    }
   
  }
