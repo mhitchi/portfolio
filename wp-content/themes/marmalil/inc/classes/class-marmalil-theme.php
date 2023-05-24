d4314b38b2e4f5bd1740cda58fb0157eab9dc9bf
May Hitchings <mayhitchings@gmail.com>
2023-05-24 07:27:15 -0400

    adding image links

--- wp-content/themes/marmalil/inc/classes/class-marmalil-theme.php
+++ wp-content/themes/marmalil/inc/classes/class-marmalil-theme.php
@@ -47,7 +47,7 @@
 
       add_theme_support( 'custom-background', [
          'default-color' => '#f7f2ea',
-         'default-image' => get_template_directory_uri() . '/images/wapuu.jpg',
+         'default-image' => get_template_directory_uri() . 'http://localhost:8888/wordpress/wp-content/uploads/2023/05/letters-print-1.webp',
          'default-repeat'=> 'no-repeat',
       ] );
    }
