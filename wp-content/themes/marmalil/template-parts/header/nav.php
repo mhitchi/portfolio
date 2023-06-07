94f512cc24ebe6244276c8359744928ccc478301
May Hitchings <mayhitchings@gmail.com>
2023-06-07 09:02:16 -0400

    adding conditionals for child menu support

--- wp-content/themes/marmalil/template-parts/header/nav.php
+++ wp-content/themes/marmalil/template-parts/header/nav.php
@@ -23,24 +23,21 @@
                 <i class="fas fa-bars text-white"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <?php
+                    if ( ! empty( $header_menus ) && is_array( $header_menus ) ) {
+                    ?>
                     <ul class="navbar-nav ml-auto">
-                        <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
-                        <li class="nav-item">
-                            <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Design</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Development</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Testimonials</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Contact</a>
-                        </li>
+                        <?php
+                            foreach ( $header_menus as $menu_item ) {
+                                if ( ! $menu_item->menu_item_parent ) {
+
+                                }
+                            }
+                        ?>
                     </ul>
+                    <?php 
+                    }
+                    ?>
                 </div>
             </nav>
 
