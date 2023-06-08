112967e2cc4e174a9abed3d5913c28eaa6993b0b
May Hitchings <mayhitchings@gmail.com>
2023-06-08 07:43:24 -0400

    troubleshooting, error free but menu not dynamic yet, still holding placeholder values

--- wp-content/themes/marmalil/inc/classes/class-menus.php
+++ wp-content/themes/marmalil/inc/classes/class-menus.php
@@ -47,8 +47,8 @@
 
     public function get_child_menu_items( $menu_array, $parent_id ) {
         $child_menus = [];
-        if (! empty( $menu_array ) && is_array( $menu_array ) ) {
-            for ( $menu_array as $menu ) {
+        if ( ! empty( $menu_array ) && is_array( $menu_array ) ) {
+            foreach ( $menu_array as $menu ) {
                 if ( intval( $menu->menu_item_parent ) === $parent_id ) {
                     array_push( $child_menus, $menu );
                 }
