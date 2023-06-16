5f8afac4273fbe05f6490832c868e7ae7c994b5d
May Hitchings <mayhitchings@gmail.com>
2023-06-16 08:04:48 -0400

    adding documentation

--- wp-content/themes/marmalil/index.php
+++ wp-content/themes/marmalil/index.php
@@ -28,33 +28,39 @@
                         }
                     ?>
                     <div class="row">
-                        <?php
-                            $index = 0;
-                            //set number of columns
-                            $column_count = 3;
+                    <?php
+                        $index = 0;
+                        //set number of columns
+                        $column_count = 3;
+                        //Case: index = 0;
+                        //START THE LOOP
+                        while ( have_posts() ) : the_post();
+                            //if remainder of index and column count is 0, print 3 columns (on desktop)
+                            if ( 0 === $index % $column_count ) {
+                    ?>
+                        <div class="col-lg-4 col-md-6 col-sm-12">
+                    <?php
+                            }
+            ?>              <div class="post_card">
+                                <h2><?php the_title(); ?></h2>
+                                <div class="excerpt">
+                                    <p><?php the_excerpt(); ?></p>
+                                </div>
+                            </div>
+                    <?php
+                            //increment index
+                            $index++;
 
-                            //START THE LOOP
-                            while ( have_posts() ) : the_post();
-                                if ( 0 === $index % $column_count ) {
-                        ?>
-                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                        <?php
-                                }
-                        ?>
-                                        <h3><?php the_title(); ?></h3>
-                                        <div class="excerpt">
-                                            <p><?php the_excerpt(); ?></p>
-                                        </div>
-                        <?php
-                                $index++;
-                                    if ( 0 !== $index && 0 === $index % $column_count ) {
-                        ?> 
-                                        </div>
-                        <?php
-                                    }
-                            endwhile;
-                        ?>
-                    </div>
+                            //now that index !== 0, check if it's remainder with the column count is 0,
+                            //if not, continue iterating through columns
+                            if ( 0 !== $index && 0 === $index % $column_count ) {
+                    ?> 
+                        </div> <!--col-->
+                    <?php
+                            }
+                        endwhile;
+                    ?>
+                    </div> <!-- row -->
                     plan: https://codepen.io/eorlandno/pen/LYYpypz
                 </div>
                 <?php
