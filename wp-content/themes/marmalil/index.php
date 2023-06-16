057cd022f5fe97e5b3033db82d6f937f87811c41
May Hitchings <mayhitchings@gmail.com>
2023-06-16 07:56:18 -0400

    adding bootstrap grid

--- wp-content/themes/marmalil/index.php
+++ wp-content/themes/marmalil/index.php
@@ -20,19 +20,41 @@
                         if ( is_home() && ! is_front_page() ) {
                     ?>
                         <header class="mb-5">
-                            <h1 class="page-tit;e screen-reader-text">
+                            <h1 class="page-title screen-reader-text">
                                 <?php single_post_title()?>
                             </h1>
                         </header>
                     <?php
                         }
                     ?>
-                    <?php
-                        while ( have_posts() ) : the_post();
-                            the_title();
-                            the_content();
-                        endwhile;
-                    ?>
+                    <div class="row">
+                        <?php
+                            $index = 0;
+                            //set number of columns
+                            $column_count = 3;
+
+                            //START THE LOOP
+                            while ( have_posts() ) : the_post();
+                                if ( 0 === $index % $column_count ) {
+                        ?>
+                                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <?php
+                                }
+                        ?>
+                                        <h3><?php the_title(); ?></h3>
+                                        <div class="excerpt">
+                                            <p><?php the_excerpt(); ?></p>
+                                        </div>
+                        <?php
+                                $index++;
+                                    if ( 0 !== $index && 0 === $index % $column_count ) {
+                        ?> 
+                                        </div>
+                        <?php
+                                    }
+                            endwhile;
+                        ?>
+                    </div>
                     plan: https://codepen.io/eorlandno/pen/LYYpypz
                 </div>
                 <?php
