ca805dc2abbb69636b37b0f070445dd42e8cd12a
May Hitchings <mayhitchings@gmail.com>
2023-06-16 07:34:32 -0400

    getting post title

--- wp-content/themes/marmalil/index.php
+++ wp-content/themes/marmalil/index.php
@@ -14,12 +14,26 @@
             if ( have_posts() ) {
                 ?>
                 <div class="container">
+                    <?php 
+                    // checks if page is home (homepage or blog home)
+                    //if it's home AND it isn't front page
+                        if ( is_home() && ! is_front_page() ) {
+                    ?>
+                        <header class="mb-5">
+                            <h1 class="page-tit;e screen-reader-text">
+                                <?php single_post_title()?>
+                            </h1>
+                        </header>
+                    <?php
+                        }
+                    ?>
                     <?php
                         while ( have_posts() ) : the_post();
                             the_title();
                             the_content();
                         endwhile;
                     ?>
+                    plan: https://codepen.io/eorlandno/pen/LYYpypz
                 </div>
                 <?php
             }
