f730b71c356d7b4bcc805b4f88425983746ffbd5
May Hitchings <mayhitchings@gmail.com>
2023-06-20 07:38:05 -0400

    fleshing out empty template

--- wp-content/themes/marmalil/template-parts/content-none.php
+++ wp-content/themes/marmalil/template-parts/content-none.php
@@ -2,13 +2,15 @@
 
 /**
  * 
- * Empty content
+ * Template part for displaying a message that posts cannot be found
  * 
  * @package Marmalil
  */
 
  ?>
 
- <div>
-    <h1>This page is empty</h1>
-</div>
+ <section class="no-result not-found">
+    <header class="page-header">
+        <h1 class="page-title"><?php esc_html_e( 'No Posts Found', 'marmalil' ); ?></h1>
+    </header>
+ </section>
