db5bc68eddaf76fa022df2f65bef174bb9cf2b3b
May Hitchings <mayhitchings@gmail.com>
2023-06-20 07:47:29 -0400

    add ability to post first post if admin in empty content template

--- wp-content/themes/marmalil/template-parts/content-none.php
+++ wp-content/themes/marmalil/template-parts/content-none.php
@@ -13,4 +13,32 @@
     <header class="page-header">
         <h1 class="page-title"><?php esc_html_e( 'No Posts Found', 'marmalil' ); ?></h1>
     </header>
+
+
+    <div class="page-content">
+        <?php
+            /*if it's the blog homepage and user can publish posts*/
+            if ( is_home() && current_user_can( 'publish_posts' ) ) {
+        ?>
+                <p>
+                    <?php
+                        printf(
+                            /*strips disallowed html*/
+                            /*string printed, allowed html */
+                            wp_kses(  
+                                __( 'Ready to publish your first post? <a href="%s"> Get started here</a>', 'marmalil' ),
+                                [
+                                    'a' => [
+                                        'href' => []
+                                    ]
+                                ]
+                            ),
+                            esc_url( admin_url( 'post-new.php' ) ))
+                        )
+                    ?>
+                </p>
+        <?php
+            }
+        ?>
+    </div>
  </section>
