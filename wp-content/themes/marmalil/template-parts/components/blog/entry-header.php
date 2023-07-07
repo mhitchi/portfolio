e79b834033835e76c8811c239aa849e27a1a7e2b
May Hitchings <mayhitchings@gmail.com>
2023-07-07 07:39:44 -0400

    starting on post header template

---
+++ wp-content/themes/marmalil/template-parts/components/blog/entry-header.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template for post entry header
+ * 
+ * @package marmalil
+ */
+
+ $the_post_id = get_the_ID();
+ $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );
+ ?>
+
+ <header class="entry-header">
+    <?php
+    //featured image
+    if ( $has_post_thumbnail ) {
+        ?>
+        <div class="entry-image mb-3">
+            <a href="<?php echo esc_url( get_permalink() ); ?>">
+            </a>
+         </div>
+         <?php
+    }
+
+    ?>
