4eadc06cb3fd413eab1d4b4d5214e237ab0c4764
May Hitchings <mayhitchings@gmail.com>
2023-07-12 07:49:28 -0400

    checking all files for ft img var name

--- wp-content/themes/marmalil/inc/helpers/template-tags.php
+++ wp-content/themes/marmalil/inc/helpers/template-tags.php
@@ -1,6 +1,6 @@
 <?php
 
-function get_the_post_custom_thumbnail( $post_id, $size = 'featured-image', $additional_attributes = [], ) {
+function get_the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', $additional_attributes = [], ) {
     $custom_thumbnail = '';
 
     if ( null === $post_id ) {
@@ -26,6 +26,6 @@ function get_the_post_custom_thumbnail( $post_id, $size = 'featured-image', $add
     return $custom_thumbnail;
 }
 
-function the_post_custom_thumbnail( $post_id, $size = 'featured-image', $additional_attributes = [], ) {
+function the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', $additional_attributes = [], ) {
     echo get_the_post_thumbnail( $post_id, $size, $additional_attributes );
 }
