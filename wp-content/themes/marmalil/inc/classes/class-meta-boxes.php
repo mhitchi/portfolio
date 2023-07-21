9da7b03edadf1031ce8c4953f381f3c2c9413253
May Hitchings <mayhitchings@gmail.com>
2023-07-21 08:11:29 -0400

    added nonce verification to custom meta box form

--- wp-content/themes/marmalil/inc/classes/class-meta-boxes.php
+++ wp-content/themes/marmalil/inc/classes/class-meta-boxes.php
@@ -43,7 +43,12 @@
 
     public function custom_meta_box_html( $post ) {
         $value = get_post_meta( $post->ID, '_hide_page_title', true );
+        /* use nonce for verification
+        */
+
+        wp_nonce_field( plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name' );
         ?>
+        
         <label for="marmalil-field"><?php esc_html_e( 'Hide the page title' ); ?></label>
         <select name="marmalil_hide_title_field" id="marmalil-field" class="postbox">
             <option value=""><?php esc_html_e( 'Select', 'marmalil' );?> </option>
@@ -58,6 +63,22 @@
     }
 
     public function save_post_meta_data( $post_id ) {
+        /**
+         *  when the post is saved or updated, we get $_POST available
+         * and we check if current user is authorized
+         * */ 
+        if ( ! current_user_can( 'edit_post', $post_id) ) {
+            return;
+        }
+
+        /**
+         * check if the nonce value we received is the same we created
+         */
+        if ( ! isset( $_POST['hide_title_meta_box_nonce_name'] ) ||
+            ! wp_verify_nonce( plugin_basename( __FILE__), $_POST[ 'hide_title_meta_box_nonce_name' ])
+            ) {
+                return;
+        }
         if(array_key_exists('marmalil_hide_title_field', $_POST)) {
             update_post_meta(
                 $post_id,
