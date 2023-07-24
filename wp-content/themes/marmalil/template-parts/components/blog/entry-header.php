44c0b1bfb1b310a030e21c9c17d98de596f5a91c
May Hitchings <mayhitchings@gmail.com>
2023-07-24 07:54:13 -0400

    conditionallly dsiplaying page or post title

--- wp-content/themes/marmalil/template-parts/components/blog/entry-header.php
+++ wp-content/themes/marmalil/template-parts/components/blog/entry-header.php
@@ -8,7 +8,8 @@
  
  $the_post_id = get_the_ID();
  $hide_title get_post_meta( $the_post_id, '_hide_page_title', true );
- 
+ $heading_class = ! empty( $hide_title ) && 'yes' === $hide_title ? 'hide' : '';
+
  $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );
  ?>
 
@@ -31,5 +32,15 @@
             ]
          )
     }
-
+//title
+//if on a single page or reg page
+if ( is_single() || is_page() ) {
+    printf( 
+        '<h1 class="page-title text-dark %1$s">%2$s</h1>',
+        esc_attr( $heading_class ),
+        // sanitizes
+        wp_kses_post( $get_the_title() )
+    )
+}
     ?>
+    </header>
