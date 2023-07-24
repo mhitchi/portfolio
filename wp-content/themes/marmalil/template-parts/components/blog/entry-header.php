66f4d7b2249a022f611d4221da2c1322de30d94a
May Hitchings <mayhitchings@gmail.com>
2023-07-24 07:56:27 -0400

    adding card link

--- wp-content/themes/marmalil/template-parts/components/blog/entry-header.php
+++ wp-content/themes/marmalil/template-parts/components/blog/entry-header.php
@@ -39,8 +39,15 @@ if ( is_single() || is_page() ) {
         '<h1 class="page-title text-dark %1$s">%2$s</h1>',
         esc_attr( $heading_class ),
         // sanitizes
-        wp_kses_post( $get_the_title() )
-    )
+        wp_kses_post( get_the_title() )
+    );
+} else {
+    printf( 
+        '<h2 class="entry-title mb-3"><a href="%1$s">%2$s</a></h2>',
+        esc_url( get_the_permalink() ),
+        // sanitizes
+        wp_kses_post( get_the_title() )
+    );
 }
     ?>
     </header>
