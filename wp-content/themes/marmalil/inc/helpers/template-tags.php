afd58d4dbbd92f7e855a168f5f8d16ef02c1520f
May Hitchings <mayhitchings@gmail.com>
2023-07-25 07:41:24 -0400

    getting a hot date

--- wp-content/themes/marmalil/inc/helpers/template-tags.php
+++ wp-content/themes/marmalil/inc/helpers/template-tags.php
@@ -33,7 +33,16 @@ function the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', $add
 function marmalil_posted_on() {
     $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
+    // Post is modified
     if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' )) {
-        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
     }
+
+
+    $time_string = sprintf( $time_string, 
+    esc_attr( get_the_date( DATE_W3C ) ),
+    esc_attr( get_the_date() ),
+    esc_attr( get_the_modified_date( DATE_W3C ) ),
+    esc_attr( get_the_modified_date() ),
+);
 }
