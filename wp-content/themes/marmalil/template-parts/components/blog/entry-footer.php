c133719b205f5b72f0587eabeaf6bfcb843d3708
May Hitchings <mayhitchings@gmail.com>
2023-08-09 07:15:10 -0400

    fixed entry footer links

--- wp-content/themes/marmalil/template-parts/components/blog/entry-footer.php
+++ wp-content/themes/marmalil/template-parts/components/blog/entry-footer.php
@@ -20,7 +20,7 @@ $article_terms = wp_get_post_terms( $the_post_id, [ 'category', 'post_tag'] );
         foreach ( $article_terms as $key => $article_term ) {
     ?>
         <button class="btn border border-secondary mb-2 mr-2">
-            <a class="entry-footer-link text-black-50" href="<?php echo esc_url( get_the_term_link( $article_term) )?>">
+            <a class="entry-footer-link text-black-50" href="<?php echo esc_url( get_term_link( $article_term) )?>">
                 <?php echo esc_html( $article_term->name )?>
             </a>
         </button>
