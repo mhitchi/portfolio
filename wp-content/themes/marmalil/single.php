b0729e2ddc994a8eda349ed2725eac79d51868c0
May Hitchings <mayhitchings@gmail.com>
2023-08-14 07:49:02 -0400

    adding prev and next buttons to single post

--- wp-content/themes/marmalil/single.php
+++ wp-content/themes/marmalil/single.php
@@ -34,7 +34,8 @@
                 /*.php file extention not needed on this next part*/
                 get_template_part( 'template-parts/content-none' );
                     endwhile;
-                    marmalil_pagination();
+                    previous_post_link();
+                    next_post_link();
         ?>
 
     </main>
