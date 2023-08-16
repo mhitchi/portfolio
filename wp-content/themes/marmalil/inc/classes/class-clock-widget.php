959072b4fbc71dae81ea217a707cd6843de5bb8b
May Hitchings <mayhitchings@gmail.com>
2023-08-16 07:52:35 -0400

    adding structure for clock widget

--- wp-content/themes/marmalil/inc/classes/class-clock-widget.php
+++ wp-content/themes/marmalil/inc/classes/class-clock-widget.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Enqueue theme assets
+ * Clock Widget
  * 
  * @package Marmalil
  * 
@@ -9,22 +9,75 @@
  namespace MARMALIL_THEME\Inc;
 
  use MARMALIL_THEME\Inc\Traits\Singleton;
+ use WP_Widget;
 
- class Assets {
-    use Singleton;
+ class Clock_Widget extends WP_Widget {
 
-    protected function __construct() {
-        // load other classes.
-        Assets::get_instance();
+	use Singleton;
 
-        // load hooks
-        $this->setup_hooks();
-    }
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+			'clock_widget', // Base ID
+			'Clock', // Name
+			[ 'description' => __( 'Clock Widget', 'aquila' ), ] // Args
+		);
+	}
 
-    protected function setup_hooks() {
-        // actions and filter
-        
-    }
+	/**
+	 * Front-end display of widget.
+	 */
+	public function widget( $args, $instance ) {
+		extract( $args );
 
-   
- }
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $before_widget;
+
+		if ( ! empty( $title ) ) {
+			echo $before_title . $title . $after_title;
+		}
+		?>
+		<section class="card">
+			<div class="clock card-body">
+				<span id="time"></span>
+				<span id="ampm"></span>
+				<span id="time-emoji"></span>
+			</div>
+		</section>
+		<?php
+		echo $after_widget;
+	}
+
+	/**
+	 * Back-end widget form.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance['title'] ) ) {
+			$title = $instance['title'];
+		} else {
+			$title = __( 'New title', 'aquila' );
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:', 'aquila' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text"
+			       value="<?php echo esc_attr( $title ); ?>"/>
+		</p>
+		<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance          = [];
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+}
