4f8ad23b71ff64feb2eeacdd557d7f7f5ed99745
May Hitchings <mayhitchings@gmail.com>
2023-08-24 07:59:21 -0400

    adding dependencies

--- wp-content/themes/marmalil/assets/src/webpack.config.js
+++ wp-content/themes/marmalil/assets/src/webpack.config.js
@@ -1,4 +1,15 @@
+const path = require( 'path' );
+
+const JS_DIR = path.resolve( __dirname, '');
+
+const entry = {
+    main:
+};
+const output = {};
+
 module.exports = ( env, argv ) => ({
+    entry: entry,
+    output: output,
 
 })
-//usually would export an object, but in this situation we want it inside a function so that it can work for multiple environments???
+//usually would export an object, but in this situation we want it inside a function so that it can work for multiple environments
