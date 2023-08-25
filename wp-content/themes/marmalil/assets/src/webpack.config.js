fb7ca79529c210b7b09dec670f9f56b8e723f414
May Hitchings <mayhitchings@gmail.com>
2023-08-25 08:28:37 -0400

    continued

--- wp-content/themes/marmalil/assets/src/webpack.config.js
+++ wp-content/themes/marmalil/assets/src/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require( 'path' );
 
 const JS_DIR = path.resolve( __dirname, '/src/js');
+const IMG_DIR = path.resolve( __dirname, '/src/img');
+const BUILD_DIR = path.resolve( __dirname, 'build');
 
 const entry = {
     main: JS_DIR + '/main.js',
