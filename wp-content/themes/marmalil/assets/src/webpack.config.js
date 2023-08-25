a8a9ee61accd30eda59c883c4f86cf716ee1a274
May Hitchings <mayhitchings@gmail.com>
2023-08-25 08:27:46 -0400

    adding entry points to webpack config

--- wp-content/themes/marmalil/assets/src/webpack.config.js
+++ wp-content/themes/marmalil/assets/src/webpack.config.js
@@ -3,7 +3,8 @@ const path = require( 'path' );
 const JS_DIR = path.resolve( __dirname, '/src/js');
 
 const entry = {
-    main:
+    main: JS_DIR + '/main.js',
+    single: JS_DIR + '/single.js',
 };
 const output = {};
 
